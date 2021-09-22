ddf924427bd365acf3a5cd3091611cc70997797f
IlhamIzaz <ilhamkyuubi@gmail.com>
2021-09-22 22:57:45 +0700

    upload

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+	public function index()
+	{
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Ilham Izaz Amanullah
+ Saya tinggal di daerah Cikaarang
+ olahraga yang saya sukai adalah
+ Basket";
+ }
+}
+?>

9e2431292474402525e163dc56f4839c698a1abe
eraserlyan <eraserlyana5@gmail.com>
2021-09-30 21:08:44 +0700

    first commit

--- app/Controllers/Contoh1.php
+++ app/Controllers/Contoh1.php
@@ -5,10 +5,10 @@ class Contoh1 extends BaseController
 	public function index()
 	{
  echo "<h1>Perkenalkan</h1>";
- echo"Nama saya Ilham Izaz Amanullah
- Saya tinggal di daerah Cikaarang
+ echo"Nama saya Era Serly Ana
+ Saya tinggal di daerah Bekasi Timur
  olahraga yang saya sukai adalah
- Basket";
+ Badminton";
  }
 }
 ?>
